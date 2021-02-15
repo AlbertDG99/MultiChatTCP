593b0b6e565a2fa8f6764c230ae6a0fb1b8bdb72
Alvarwow <alvaroherencias@hotmail.com>
2021-02-15 20:15:24 +0100

    prechat

--- src/com/company/Chat.java
+++ src/com/company/Chat.java
@@ -1,4 +1,14 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Chat {
+    private JTextArea tChat;
+    private JPanel panel1;
+    private JButton bEnviar;
+    private JTextField tMensaje;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
